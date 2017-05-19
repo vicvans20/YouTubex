dac125ba2606d1d4beb85a998a43bf38b8e4356d
vicvans20 <vicvans20@gmail.com>
2017-05-19 12:46:47 -0600

    Refractor code for different resources. Add possibility to set access_token to fetch personal data.

--- lib/youtubex.ex
+++ lib/youtubex.ex
@@ -1,6 +1,10 @@
 defmodule Youtubex do
+
+  @base_url "https://www.googleapis.com/youtube/v3/"
+
   @initial_state %{
     api_key: nil,
+    access_token: nil,
   }
   @moduledoc """
   Documentation for Youtubex.
@@ -11,20 +15,20 @@ defmodule Youtubex do
   ############ API ###########
   ############################
 
-  def start_link(api_key \\ :none, opts \\ []) do
-    GenServer.start_link(__MODULE__, {api_key, opts}, name: __MODULE__)
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
-  def search(term, max_results \\ 10) do
-    GenServer.call(__MODULE__, {:search, term, max_results})
+  def search(term) do
+    GenServer.call(__MODULE__, {:search, term})
   end
 
-  def search_channel(display_name, max_results \\ 10) do
-    GenServer.call(__MODULE__, {:search_channel, display_name, max_results})
+  def search_channel(display_name) do
+    GenServer.call(__MODULE__, {:search_channel, display_name})
   end
 
-  def my_videos() do
-    GenServer.call(__MODULE__, :my_videos)
+  def my_channel() do
+    GenServer.call(__MODULE__, :my_channel)
   end
 
   def test() do
@@ -38,31 +42,34 @@ defmodule Youtubex do
   ############################################
   ############# Callback Functions ###########
   ############################################
-  def init({api_key, _opts}) do
-    state = Map.put(@initial_state, :api_key, api_key)
+  def init(opts) do
+    state = @initial_state
+      |> Map.put(:api_key, opts[:api_key] || :none)
+      |> Map.put(:access_token, opts[:access_token] || :none)
     {:ok, state}
   end
 
-  def handle_call({:search, term, max_results}, _from, state) do
+  def handle_call({:search, term}, _from, state) do
     api_key = state[:api_key]
-    url = gen_request_url(api_key, :search, %{max_results: max_results , query: term})
-    IO.puts "Checking this: \n #{url}"
-    result = HTTPoison.get(url) |> response_parser
-    {:reply, result, state}
-  end
-
-  def handle_call({:search_channel, term, max_results}, _from, state) do
-    api_key = state[:api_key]
-    url = gen_request_url(api_key, :search, %{max_results: max_results , type: :channel, query: term})
+    url = gen_request_url(api_key, :search, %{query: term})
 
     IO.puts "Checking this: \n #{url}"
     result = HTTPoison.get(url) |> response_parser
     {:reply, result, state}
   end
 
-  def handle_call(:my_videos, _from, state) do
+  def handle_call({:search_channel, term}, _from, state) do
     api_key = state[:api_key]
-    url = gen_request_url(api_key, :channel, %{action: :mine})
+    url = gen_request_url(api_key, :search, %{type: :channel, query: term})
+
+    IO.puts "Checking this: \n #{url}"
+    result = HTTPoison.get(url) |> response_parser
+    {:reply, result, state}
+  end
+
+  def handle_call(:my_channel, _from, state) do
+    token = state[:access_token]
+    url = gen_request_url(:oauth2, token, :channels, %{action: :mine})
     IO.puts "Checking this: \n #{url}"
     result = HTTPoison.get(url) |> response_parser
     {:reply, result, state}
@@ -70,7 +77,8 @@ defmodule Youtubex do
 
   def handle_call(:test, _from, state) do
     api_key = state[:api_key]
-    url = "https://www.googleapis.com/youtube/v3/channels?key=#{api_key}&part=id&forUsername=eveevans"
+    # url = "https://www.googleapis.com/youtube/v3/channels?key=#{api_key}&part=id&forUsername=eveevans"
+    url = "https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&playlistId=WL&access_token=ya29.GlxPBPvdat29lSZ5YgxDm7tgF-cCylRViAuEq-DHQE0wG0DWQ9fGOvVzvB2IU9kDEYBwkq9A7oqdP-G9hq76KzhHFsBsWAT9akvJbUTWGwN8VFWZbVas_U5Ci1FBwQ"
     result = HTTPoison.get(url) |> response_parser
     {:reply, result, state}
   end
@@ -83,54 +91,68 @@ defmodule Youtubex do
   ############ Helper Functions###############
   ############################################
 
+  # For oauth2 required resources
+  defp gen_request_url(:oauth2, token, resource, params = %{}) do
+    request_params = parse_params(params)
+    request_url = build_resource_request(resource, request_params)
+    <> "&access_token=#{token}"
+    request_url
+  end
+
+  # With api key
   defp gen_request_url(api_key, resource, params = %{}) do
-    resource_param = parse_resource(resource)
-    api_key_param = parse_api_key(api_key)
-    base_url = "https://www.googleapis.com/youtube/v3/" <> resource_param <> api_key_param
-    complete_request(base_url, resource, params)
+    request_params = parse_params(params)
+    request_url = build_resource_request(resource, request_params)
+    <> "&key=#{api_key}"
+    request_url
+  end
+  
+  # Without api key
+  defp gen_request_url(:none, resource, params = %{}) do
+    request_params = parse_params(params)
+    build_resource_request(resource, request_params)
   end
 
-  defp parse_resource(resource) do
-    case resource do
-      :search -> "search?"
-      :channel -> "channels?"
-    end
-  end
 
-  defp parse_api_key(api_key) do
-    case api_key do
-      :none -> ""
-      key -> "key=#{key}"
-    end
-  end
-
-  defp complete_request(base_url, :search, %{max_results: max_results, query: term}) do
-    request_url = base_url
-    <> "&part=snippet"
+  # Build base resource url request according to the endpoint
+  defp build_resource_request(:search, %{max_results: max_results, part: part ,query: term}) do
+    request_url = @base_url
+    <> "search?"
+    <> "part=#{part}"
     <> "&maxResults=#{max_results}"
     <> "&query=#{term}"
     request_url
   end
 
-  defp complete_request(base_url, :search, %{max_results: max_results, type: :channel ,query: term}) do
-    request_url = base_url
-    <> "&part=snippet"
+  defp build_resource_request(:search, %{max_results: max_results, part: part ,type: :channel ,query: term}) do
+    request_url = @base_url
+    <> "search?"
+    <> "part=#{part}"
     <> "&maxResults=#{max_results}"
     <> "&type=channel"
     <> "&query=#{term}"
     request_url
   end
 
-  defp complete_request(base_url, :channel, %{action: :mine}) do
-    request_url = base_url
-    <> "&part=contentDetails"
+  defp build_resource_request(:channels, %{action: :mine}) do
+    request_url = @base_url
+    <> "channels?"
+    <> "part=contentDetails"
     <> "&mine=true"
     request_url
   end
 
-  defp complete_request(base_url, :channel, %{action: _action}) do
-    request_url = base_url
+  defp build_resource_request(:channel, _any) do
+    request_url = @base_url
     request_url
+  end
+
+  defp parse_params(params) do
+    parsed_params = params # Get extra non-default params like term
+    parsed_params = parsed_params 
+      |> Map.put(:max_results, params[:max_results] || 10)
+      |> Map.put(:part, params[:part] || "snippet")
+    parsed_params
   end
 
 
